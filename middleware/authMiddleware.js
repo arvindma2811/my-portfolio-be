b8dfcf1641246af6c3eeca6c1392a02b27f49e42
Arvind M A <arvindma281107@gmail.com>
2026-02-25 08:40:56 +0530

    commmit 7

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,16 +1,20 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-    const token = req.header("Authorization");
+  try {
+    const authHeader = req.headers.authorization;
 
-    if (!token)
-        return res.status(401).json({ message: "Access Denied" });
-
-    try {
-        const verified = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = verified;
-        next();
-    } catch {
-        res.status(400).json({ message: "Invalid Token" });
+    if (!authHeader) {
+      return res.status(401).json({ message: "No token provided" });
     }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).json({ message: "Invalid Token" });
+  }
 };
